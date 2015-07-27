d61b330c16851b1160ece68de98eea128ad20501
cjwddtc <leeshengyuan@live.com>
2015-07-27 16:08:52 +0800

    asd

--- emenytankshow.cpp
+++ emenytankshow.cpp
@@ -3,24 +3,41 @@
 #include "main.h"
 
 namespace view{
-	TankAShow::TankAShow(bool is_red, int rat):Show(rat){
+	TankAShow::TankAShow(bool is_red, int rat,bool is_fast):Show(rat){
 		this->is_red=is_red;
+		this->is_fast=is_fast;
 	}
 	void TankAShow::move(int x, int y, ...){
 		if(x==-1&&y==-1){
 			va_list va;
 			va_start(va,y);
-			this->drt=va_arg(va,direct);
+			drt=va_arg(va,direct);
 			va_end(va);
 			countss++;
+			movedrt(drt,rec,rat);
 		}else{
-			this
+			rec.left=x;
+			rec.top=y;
+			rec.bottom=y+32;
+			rec.right=x+32;
+			va_list va;
+			va_start(va,y);
+			drt=va_arg(va,direct);
+			va_end(va);
 		}
 	}
 	void TankAShow::Repaint(){
-		if(is_red && counts&0x1){
-			RECT r;
-			movedrt(drt,r,1);
+		int n=0;
+		if(is_red && counts&0x2){
+			n|=0x2;
 		}
+		if(countss&0x1){
+			n|=0x1;
+		}
+		if(is_fast){
+			n|=0x4;
+		}
+		d3ddev->StretchRect(enemy[drt][n],NULL,backbuffer,&rec,D3DTEXF_NONE);
+		counts++;
 	}
 }
--- enemytankshow.h
+++ enemytankshow.h
@@ -1,13 +1,18 @@
+#ifndef EME_H
+#define EME_H
 #include "view.h"
 
 namespace view{
 	class TankAShow:public Show{
+	public:
 		bool is_red;
+		bool is_fast;
 		int counts;
 		int countss;
 		direct drt;
-		TankAShow(bool is_red,int rat);
+		TankAShow(bool is_red,int rat,bool is_fast);
 		virtual void Repaint();
 		virtual void move(int x, int y,...);
 	};
 }
+#endif
